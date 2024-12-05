35a94e0f280a28b181f26dd513dc4b79a1c1cd90
SirConceptz <mateusz.hermanowicz@icloud.com>
2024-12-05 21:14:21 +0100

    Add translates and try/catch to import/export db
    -add toast widget
    -add my logger

---
+++ lib/utils/my_logger.dart
@@ -0,0 +1,46 @@
+import 'dart:async';
+import 'dart:io';
+
+import 'package:intl/intl.dart';
+import 'package:package_info_plus/package_info_plus.dart';
+import 'package:path_provider/path_provider.dart';
+
+class MyLogger {
+
+  MyLogger();
+
+  static Future<void> write(String tag, String message) async {
+    var parsedDate = DateTime.now();
+    final String filenamePrefix = "SMART_BUDGET_LOG";
+    final String extension = ".txt";
+    PackageInfo packageInfo = await PackageInfo.fromPlatform();
+    String appVersion = packageInfo.version;
+    Directory externalStorageDirectory =
+    await getApplicationDocumentsDirectory();
+    String path = externalStorageDirectory.path;
+    String fileName = filenamePrefix + appVersion + extension;
+    String date = DateFormat("dd.MM.yyyy HH.mm.ss.SSS").format(parsedDate);
+    String newLog = "$date : $tag : $message\n";
+    final file = File('$path/$fileName');
+    await file.writeAsString(newLog, mode: FileMode.append);
+  }
+
+  Future<String> get _localPath async {
+    final directory = await getApplicationDocumentsDirectory();
+    return directory.path;
+  }
+
+  Future<String?> getFileUri() async {
+    PackageInfo packageInfo = await PackageInfo.fromPlatform();
+    final String filenamePrefix = "SMART_BUDGET_LOG";
+    final String extension = ".txt";
+    String appVersion = packageInfo.version;
+    String directoryPath = await _localPath;
+    String path = "$directoryPath/$filenamePrefix$appVersion$extension";
+    if (await File(path).exists()) {
+      return path;
+    } else {
+      return null;
+    }
+  }
+}
