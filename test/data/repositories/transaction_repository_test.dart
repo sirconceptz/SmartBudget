11d792c623a54f6fb40a1f107677b56771c89a23
SirConceptz <mateusz.hermanowicz@icloud.com>
2024-12-09 13:48:33 +0100

    Add send logs from application

---
+++ test/data/repositories/transaction_repository_test.dart
@@ -0,0 +1,6 @@
+import 'package:mockito/annotations.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:smart_budget/data/db/database_helper.dart';
+
+@GenerateMocks([DatabaseHelper, Database])
+void main() {}
--- test/mocks/database_helper_mocks.dart
+++ test/mocks/database_helper_mocks.dart
@@ -1,6 +1,5 @@
 import 'package:mockito/annotations.dart';
 import 'package:smart_budget/data/db/database_helper.dart';
-import 'package:sqflite/sqflite.dart';
 
-@GenerateMocks([DatabaseHelper, Database])
+@GenerateMocks([DatabaseHelper])
 void main() {}
